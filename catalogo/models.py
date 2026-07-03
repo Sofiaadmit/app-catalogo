083eb8d07d2e66871f49fd55b08e05e4a96a0d48
SofiaCruz <isabele.sofia23@gmail.com>
2026-07-02 21:50:56 -0300

    02.07

--- catalogo/models.py
+++ catalogo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Obra(models.Model):
+    TIPO_CHOICES = [
+        ("Filme", "Filme"),
+        ("Série", "Série"),
+    ]
+
+    tiulo = models.CharField(max_length=150) # quantidade de caracteres
+    tipo = models.CharField(max_length=10, choices=TIPO_CHOICES)
+    ano = models.IntegerField()
+    genero = models.CharField(max_length=100)
+    descricao = models.TextField() #permite criar um bloco de texto 
+
+    def __str__(self):
+        return self.titulo 
+
+
